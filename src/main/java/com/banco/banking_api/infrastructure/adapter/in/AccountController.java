7cb308e292ad6bfd7b379dc4174b9a9610fbec85
ccaristi <ccaristi@bancolombia.com.co>
2025-05-02 15:17:16 -0500

    se implementa swagger openapi

--- src/main/java/com/banco/banking_api/infrastructure/adapter/in/AccountController.java
+++ src/main/java/com/banco/banking_api/infrastructure/adapter/in/AccountController.java
@@ -2,6 +2,7 @@ package com.banco.banking_api.infrastructure.adapter.in;
 
 import com.banco.banking_api.domain.model.Account;
 import com.banco.banking_api.domain.port.in.AccountUseCase;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,11 +22,13 @@ public class AccountController {
 
 
     @PostMapping
+    @Operation(summary = "Crea nueva cuenta")
     public ResponseEntity<Account> create(@RequestBody Account account) {
         return ResponseEntity.ok(useCase.create(account));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Consulta la cuenta por ID")
     public ResponseEntity<Account> get(@PathVariable Long id) {
         return useCase.getById(id)
                 .map(ResponseEntity::ok)
@@ -33,16 +36,19 @@ public class AccountController {
     }
 
     @GetMapping
+    @Operation(summary = "Consulta todas las cuentas")
     public List<Account> getAll() {
         return useCase.getAll();
     }
 
     @PutMapping
+    @Operation(summary = "Actualiza la cuenta")
     public ResponseEntity<Account> update(@RequestBody Account account) {
         return ResponseEntity.ok(useCase.update(account));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Elimina la cuenta por ID")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         useCase.delete(id);
         return ResponseEntity.noContent().build();
--- src/main/java/com/banco/banking_api/infrastructure/adapter/in/TransactionController.java
+++ src/main/java/com/banco/banking_api/infrastructure/adapter/in/TransactionController.java
@@ -21,25 +21,25 @@ public class TransactionController {
     }
 
     @PostMapping
-    @Operation(summary = "Create a new transaction")
+    @Operation(summary = "Crea nueva transacción")
     public ResponseEntity<Transaction> create(@RequestBody Transaction transaction) {
         return ResponseEntity.ok(transactionUseCase.createTransaction(transaction));
     }
 
     @GetMapping("/account/{accountId}")
-    @Operation(summary = "Get transactions by account ID")
+    @Operation(summary = "Consulta las transacción por ID de cuenta")
     public ResponseEntity<List<Transaction>> getByAccount(@PathVariable Long accountId) {
         return ResponseEntity.ok(transactionUseCase.getTransactionsByAccount(accountId));
     }
 
     @GetMapping
-    @Operation(summary = "Get all transactions")
+    @Operation(summary = "Consulta todas las transacciones")
     public ResponseEntity<List<Transaction>> getAllTransactions() {
         return ResponseEntity.ok(transactionUseCase.getAllTransactions());
     }
 
     @DeleteMapping("/{id}")
-    @Operation(summary = "Delete a transaction by ID")
+    @Operation(summary = "Eliminar la transaccion por ID")
     public ResponseEntity<Void> deleteTransaction(@PathVariable Long id) {
         transactionUseCase.deleteTransaction(id);
         return ResponseEntity.noContent().build();
